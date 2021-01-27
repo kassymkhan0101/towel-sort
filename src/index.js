166831697dc9a149d412c37d34621f247cc5b3c4
kassymkhan0101 <45078668+kassymkhan0101@users.noreply.github.com>
2021-01-28 01:16:45 +0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,11 +8,11 @@ module.exports = function towelSort (matrix) {
   for(i=1; i< matrix.length+1; i++){
       if(i%2 == 1){
       for(k=1; k< i.length; k++){
-          c.push[i[k]]
+          c.push(i[k])
       }}
       else{
       for(k=i.length; k>0; k--){
-          c.push[i[k]]
+          c.push(i[k])
       }}
   }
     return c;}
