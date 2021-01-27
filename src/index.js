821d73fc0f77095ca5cc8c3e43ee40e1b5e54ea6
kassymkhan0101 <45078668+kassymkhan0101@users.noreply.github.com>
2021-01-28 01:15:08 +0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
+    var c = []
   if(matrix.length == 0){
-  return 0}else{var c = []
+  return c}else{
   for(i=1; i< matrix.length+1; i++){
       if(i%2 == 1){
       for(k=1; k< i.length; k++){
