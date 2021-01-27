2cece5f7310d14bab7ab07a04f24ebc74e36bd84
kassymkhan0101 <45078668+kassymkhan0101@users.noreply.github.com>
2021-01-28 01:02:36 +0600

    add code to matrix

--- src/index.js
+++ src/index.js
@@ -2,5 +2,10 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  var c = []
+  for(i=0; i< matrix.length; i++){
+      for(k=0; k< i.length; k++){
+          c.push[i[k]]
+      }
+  }
 }
